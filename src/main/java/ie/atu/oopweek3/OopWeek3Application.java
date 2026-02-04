077155f7b417eed96e6b48779ba2ebadfdea694d
ParaicGavin-STUDENT <g00465477@atu.ie>
2026-02-04 16:19:03 +0000

    1

---
+++ src/main/java/ie/atu/oopweek3/OopWeek3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.oopweek3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopWeek3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopWeek3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/oopweek3/OopWeek3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.oopweek3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopWeek3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
